0bf503607b7e5dfed03f3b434d2b35f7a43371dc
mjarraya <mjarraya@student.42.fr>
2020-01-31 11:37:07 +0100

    route params

--- movies-app/app.js
+++ movies-app/app.js
@@ -9,13 +9,41 @@ app.use(express.static("public"));
 hbs.registerPartials(__dirname + "/views/partials");
 
 app.get("/", (request, response) => {
+  console.log("/");
+  response.render("index.hbs");
+});
+
+app.get("/movies", (request, response) => {
+  console.log("/movies");
   response.render("movies.hbs", {
     moviesList: moviesJSON
   });
 });
 
-app.get("/shawshank-redemption", (request, response) => {
-  response.render("movie.hbs", moviesJSON[0]);
+// app.get("/shawshank-redemption", (request, response) => {
+//   response.render("movie.hbs", moviesJSON[0]);
+// });
+
+app.get("/movies/:movieId", (request, response) => {
+  console.log("request.params: ", request.params);
+
+  // let movie;
+
+  // moviesJSON.forEach(el => {
+  //   if (el.imdbID === request.params.movieId) {
+  //     movie = el;
+  //   }
+  // });
+
+  const movieId = request.params.movieId;
+
+  const movie = moviesJSON.find(el => {
+    if (el.imdbID === movieId) {
+      return true;
+    }
+  });
+
+  response.render("movie.hbs", movie);
 });
 
 app.listen(5555);
